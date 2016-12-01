6c05c1aaa9cfc1bad4c846cd73e796fdb639c54d
Jose M. Tobar <ilovemysillybanana@gmail.com>
2016-11-30 23:34:43 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# dotnet-cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'dotnet-cookbook'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures dotnet-cookbook'
+long_description 'Installs/Configures dotnet-cookbook'
+version '0.1.0'
